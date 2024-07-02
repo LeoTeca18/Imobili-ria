039652cc6d6a775939fd2bbbd02b9c76c18ec3fa
LeoTeca18 <111018376+LeoTeca18@users.noreply.github.com>
2024-07-02 20:03:04 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Models\administrador;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdministradorController;
+use App\Http\Controllers\ClienteController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\CadastroController;
+use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,17 +21,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 #Geral-------------------------------------------------------------------
-Route::get('/', function () {
-    return view('geral/home');
-});
+Route::get('/', [HomeController::class, 'listarDestaques'])->name('cliente.imoveis');
 
 Route::get('/login', function () {
     return view('geral/login');
 });
 
+Route::post('login', [HomeController::class, 'login']);
+
+Route::post('logout', [HomeController::class, 'logout'])->name('logout');
+
+Route::post('/cada', [HomeController::class, 'store'])->name('cadastro.store');
+
+Route::get('/cadastro', [CadastroController::class, 'cadastro']);
+
+Route::post('/cadas', [CadastroController::class, 'store']);
+
+Route::get('escolha', [HomeController::class, 'showUserTypeForm'])->name('choose-user-type');
+
+Route::post('escolhar', [HomeController::class, 'escolha']);
 #Cliente-------------------------------------------------------------------
 Route::get('/cliente', function () {
     return view('cliente/iniC');
+});
+
+Route::get('/cliente', [ClienteController::class, 'listarDestaques'])->name('cliente.imoveis');
+
+Route::get('/ver', [ClienteController::class, 'pesquisar'])->name('cliente.imoveis');
+
+Route::get('/pesquisar', [ClienteController::class, 'pesquisar'])->name('pesquisar.imovel');
+
+Route::get('/info/{id}', [ClienteController::class, 'exibirDetalhes'])->name('imovel.detalhes');
+
+Route::get('/escolha', [ClienteController::class, 'escolherTipoCliente'])->name('escolha');
+
+Route::put('Cliente/{terreno}', [ClienteController::class, 'comprar'])->name('terrenos.comprar');
+
+Route::get('/pesquisa', function () {
+    return view('cliente/pesquisa');
 });
 
 Route::get('/comprados', function () {
@@ -43,62 +77,77 @@ Route::get('/alugadosC', function () {
     return view('cliente/alugados');
 });
 
-Route::get('/ver', function () {
-    return view('cliente/ver');
-});
-
-
 Route::get('/info', function () {
     return view('cliente/info');
 });
 
 #Proprietário-------------------------------------------------------------------
-Route::get('/proprietario', function () {
-    return view('proprietario/iniP');
-});
+Route::get('/proprietario/iniP', [ClienteController::class, 'listarImoveisP'])->name('proprietario.imoveis');
 
-Route::get('/adicionar', function () {
+Route::get('/proprietario/adicionar', function () {
     return view('proprietario/adicionar');
 });
 
-Route::get('/alugadosP', function () {
-    return view('proprietario/alugados');
-});
+Route::get('/proprietario/vendidosP', [ClienteController::class, 'imoveisVendidos'])->name('proprietario.vendidos');
 
-Route::get('/vendidosP', function () {
-    return view('proprietario/vendidos');
-});
+Route::get('/proprietario/alugadosP', [ClienteController::class, 'imoveisAlugados'])->name('proprietario.alugueres');
+
+Route::put('/cliente/{id}/compra', [ClienteController::class, 'compra'])->name('compra');
+
+Route::put('/cliente/{id}/alugar', [ClienteController::class, 'alugar'])->name('aluga');
+
+Route::put('/cliente/{id}/Areserva', [ClienteController::class, 'Areserva'])->name('reserva');
+
+Route::put('/cliente/{id}/Avisita', [ClienteController::class, 'Avisita'])->name('visitaC');
 
 #Negociante-------------------------------------------------------------------
-Route::get('/negociante', function () {
+Route::get('/negociante/iniN', function () {
     return view('funcionario/negociante/iniN');
 });
 
-Route::get('/vendidosN', function () {
-    return view('funcionario/negociante/vendidos');
-});
+Route::get('/negociante/iniN', [FuncionarioController::class, 'listarImoveis'])->name('negociante.imoveis');
 
-Route::get('/alugadosN', function () {
-    return view('funcionario/negociante/alugados');
-});
+Route::get('/negociante/vendidosN', [FuncionarioController::class, 'imoveisVendidos'])->name('negociante.vendidos');
 
-Route::get('/reservasN', function () {
-    return view('funcionario/negociante/reservasN');
-});
+Route::get('/negociante/alugadosN', [FuncionarioController::class, 'imoveisAlugados'])->name('negociante.alugueres');
 
-Route::get('/visitaN', function () {
-    return view('funcionario/negociante/visitasN');
-});
+Route::get('/negociante/reservasN', [FuncionarioController::class, 'imoveisReservados'])->name('negociante.reservas');
+
+Route::get('/negociante/visitaN', [FuncionarioController::class, 'imoveisVisitados'])->name('negociante.visitas');
+
+Route::put('/cliente/{id}/compra', [FuncionarioController::class, 'compra'])->name('compra');
+
+Route::put('/cliente/{id}/alugar', [FuncionarioController::class, 'alugar'])->name('aluga');
 
 #Gestor-------------------------------------------------------------------
-Route::get('/gestor', function () {
+Route::get('/gestor/iniG', function () {
     return view('funcionario/gestor/iniG');
 });
 
-Route::get('/aceites', function () {
-    return view('funcionario/gestor/aceites');
+Route::get('/gestor/iniG', [FuncionarioController::class, 'listarProprietarios'])->name('gestor.proprietarios');
+
+Route::get('/gestor/aceites', [FuncionarioController::class, 'pedidosAceites'])->name('gestor.proprietarios');
+
+Route::put('/gestor/{id}/validar-pedido', [FuncionarioController::class, 'validarPedido'])->name('validar.pedido');
+
+#ADM----------------------------------------------------------------------
+Route::middleware(['auth'])->group(function () {
+Route::get('/ADM/iniA', function () {
+    return view('ADM/iniA');
 });
 
-Route::get('/negados', function () {
-    return view('funcionario/gestor/negados');
+Route::get('/ADM/imoveis', function () {
+    return view('ADM/imoveis');
+});
+
+Route::get('/ADM/proprietarios', function () {
+    return view('ADM/proprietarios');
+});
+
+Route::get('/ADM/clientes', [AdministradorController::class, 'listarClientes'])->name('ADM.clientes');
+
+Route::get('/ADM/iniA', [AdministradorController::class, 'listarAgencias'])->name('ADM.agencias');
+
+Route::get('/ADM/imoveis', [AdministradorController::class, 'listarImoveis'])->name('ADM.imoveis');
+
 });
